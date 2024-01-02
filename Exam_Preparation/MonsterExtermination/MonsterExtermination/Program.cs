a7fa1630fc108518b552dac79a4e2ab488e5c29e
Pengustus <orlin.rizov.bg@gmail.com>
2024-01-02 22:43:50 +0200

    hw

--- Exam_Preparation/MonsterExtermination/MonsterExtermination/Program.cs
+++ Exam_Preparation/MonsterExtermination/MonsterExtermination/Program.cs
@@ -4,25 +4,46 @@
     {
         static void Main(string[] args)
         {
-            int[] armor = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
-            int[] strike = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            List<int> armor = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            List<int> attack = Console.ReadLine().Split(',').Select(int.Parse).ToList();
 
-            Stack<int> stack = new Stack<int>(armor);
-            Queue<int> queue = new Queue<int>(strike);
-            int remaining = 0;
+            int kills = 0;
+            List<int> remaining = new List<int>();
 
-            while (queue.Count > 0)
+            while (armor.Any() && attack.Any())
             {
-                if (queue.Peek() >= stack.Peek() || queue.Peek() + remaining >= stack.Peek())
+                int currentArmor = armor.First();
+                int currentAttack = attack.Last();
+
+                if (currentAttack >= currentArmor)
                 {
-                    remaining += queue.Peek() - stack.Peek();
-                    stack.Pop();
+                    kills++;
+
+                    if (attack.Count > 1)
+                    {
+                        attack[attack.Count - 2] += currentAttack - currentArmor;
+                    }
                 }
                 else
                 {
-
+                    armor[0] -= currentAttack;
+                    remaining.Insert(0, attack.Last());
                 }
+
+                armor.RemoveAt(0);
+                attack.RemoveAt(attack.Count - 1);
             }
+
+            if (remaining.Count == 0)
+            {
+                Console.WriteLine("All monsters have been killed!");
+            }
+            else
+            {
+                Console.WriteLine("The soldier has been defeated.");
+            }
+
+            Console.WriteLine($"Total monsters killed: {kills}");
         }
     }
 }
--- Exam_Preparation/TreasureHunt/TreasureHunt/Program.cs
+++ Exam_Preparation/TreasureHunt/TreasureHunt/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            
+            List<string> chest = Console.ReadLine().Split("|").ToList();
+
+            string command = Console.ReadLine();
+            while (command != "Yohoho!")
+            {
+                command = Console.ReadLine();
+                
+                string[] ints = command.Split();
+
+                switch (ints[0])
+                {
+                    case "Loot":
+
+                        for (int i = 1; i < ints.Length; i++)
+                        {
+                            string item = ints[i];
+
+                            if (!chest.Contains(item))
+                            {
+                                chest.Insert(0, item);
+                            }
+                        }
+
+                        break;
+
+                    case "Drop":
+
+                        int index = int.Parse(ints[1]);
+
+                        if (index >= 0 && index < chest.Count)
+                        {
+                            string dropped = chest[index];
+                            chest.RemoveAt(index);
+                            chest.Add(dropped);
+                        }
+
+                        break;
+
+                    case "Steal":
+
+                        int count = Math.Min(int.Parse(ints[1]), chest.Count);
+
+                        List<string> stolenItems = chest.TakeLast(count).ToList();
+
+                        chest.RemoveRange(chest.Count - count, count);
+                        Console.WriteLine(string.Join(", ", stolenItems));
+
+                        break;
+                }
+            }
+
+            double average = chest.Sum(item => item.Length) / (double)chest.Count;
+
+            if (chest.Count > 0)
+            {
+                Console.WriteLine($"Average treasure gain: {average:F2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
         }
     }
 }
